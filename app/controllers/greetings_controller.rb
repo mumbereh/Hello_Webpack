8928c3ef6c46b16f9316f5900fa106cca8afe3ef
mumbereh <siirasonh@gmail.com>
2023-12-12 08:13:27 +0300

    Fixed linter errors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,11 @@
 class GreetingsController < ApplicationController
   def random_greeting
     random_message = Message.order('RANDOM()').first
-    render json: { greeting: random_message&.content }
+
+    if random_message
+      render json: { greeting: random_message.content }
+    else
+      render json: { error: 'No messages found' }, status: :not_found
+    end
   end
 end
